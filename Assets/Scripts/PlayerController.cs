3ace4bc738fb91856e585fd1063269fbe767edaa
jyniahpicard1 <90396000+jyniahpicard1@users.noreply.github.com>
2026-09-15 19:03:26 -0400

    Updated Jiw Game
    finished game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,59 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public float thrustForce = 1f;
+    private float elapsedTime = 0f;
+    private float score = 0f;
+    public float scoreMultiplier = 10f;
+    public float thrustforce = 1f;
     Rigidbody2D rb;
+    public UIDocument uiDocument;
+    private Label scoreText;
+    private Button restartButton;
+    public GameObject explosionEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
+        restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
+        restartButton.style.display = DisplayStyle.None;
+        restartButton.clicked += ReloadScene;
     }
 
     // Update is called once per frame
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
+        Debug.Log("Score: " + score);
+        scoreText.text = "Score: " + score;
+
         if (Mouse.current.leftButton.isPressed) {
             // Calculate mouse.direction
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-            Vector2 direction = mousePos - transform.position.normalized;
+            Vector2 direction = (mousePos - transform.position).normalized;
 
             // Move player in direction of mouse
             transform.up = direction;
-            rb.AddForce(direction * thrustForce);
+            rb.AddForce(direction * thrustforce);
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        restartButton.style.display = DisplayStyle.Flex;
+    }
+    void ReloadScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
+
+
+
